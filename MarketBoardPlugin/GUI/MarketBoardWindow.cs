c26f7ef89501ab679d0c4be4d1cd34ebadf40b66
Yarukon <a2692866561@live.com>
2025-06-28 17:45:30 +0800

    acutal support cn worlds, version bump

--- MarketBoardPlugin/GUI/MarketBoardWindow.cs
+++ MarketBoardPlugin/GUI/MarketBoardWindow.cs
@@ -1081,7 +1081,7 @@ namespace MarketBoardPlugin.GUI
 
         var currentDc = localPlayer.CurrentWorld.Value.DataCenter;
         var dcWorlds = this.plugin.DataManager.GetExcelSheet<World>()
-          .Where(w => w.DataCenter.RowId == currentDc.RowId && (w.IsPublic || w.Region == 5))
+          .Where(w => w.DataCenter.RowId == currentDc.RowId && (w.IsPublic || (w.RowId > 1000 && w.UserType == 101))) // CN Servers RowId are all beyond 1000
           .OrderBy(w => w.Name.ExtractText())
           .Select(w =>
           {
